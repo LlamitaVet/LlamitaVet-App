480be6cc07264d00cb7bf2a9be179490b17342ef
Diego Osorio <diegoosorioupc@gmail.com>
2022-11-24 11:25:13 -0500

    create favorite

--- lib/screens/veterinarian_list.dart
+++ lib/screens/veterinarian_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:llamita_vet/screens/veterinarian_detail.dart';
+import 'package:llamita_vet/utils/db_helper.dart';
 import 'package:llamita_vet/utils/http_helper.dart';
 import '../models/veterinarian_model.dart';
 
@@ -15,6 +16,7 @@ class _VeterinarianListState extends State<VeterinarianList> {
   int? veterinariansCount;
   List? veterinarians;
   HttpHelper? helper;
+
 
   @override
   void initState(){
@@ -67,6 +69,9 @@ class VeterinarianItem extends StatefulWidget {
 
 class _VeterinarianItemState  extends State<VeterinarianItem> {
 
+  late bool favorite;
+  late DbHelper dbHelper;
+
   late NetworkImage image;
   final String defaultImage =
       "https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg";
@@ -74,7 +79,18 @@ class _VeterinarianItemState  extends State<VeterinarianItem> {
 
   @override
   void initState() {
+    favorite = false;
+    dbHelper = DbHelper();
+    isFavorite();
     super.initState();
+  }
+
+  Future isFavorite() async{
+    await dbHelper.openDb();
+    final result = await dbHelper.isFavorite(widget.veterinarianModel);
+    setState(() {
+      favorite = result;
+    });
   }
 
 
@@ -96,6 +112,16 @@ class _VeterinarianItemState  extends State<VeterinarianItem> {
               MaterialPageRoute(builder: (_) => VeterinarianDetail(widget.veterinarianModel));
           Navigator.push(context, route);
         },
+        trailing: IconButton(
+          icon: Icon(Icons.favorite,
+              color: favorite ? Colors.deepOrange : Colors.grey),
+          onPressed: () {
+            favorite?dbHelper.delete(widget.veterinarianModel): dbHelper.insert(widget.veterinarianModel);
+            setState(() {
+              favorite = !favorite;
+            });
+          },
+        ),
         leading: CircleAvatar(
           backgroundImage: image,
         ),
