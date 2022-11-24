6bff23db7ea1594a0e94c3d00b54c731619796b9
MaxTer43 <u201416643@upc.edu.pe>
2022-11-23 23:49:36 -0500

    Added connection to endpoint Reviews

--- lib/models/review_model.dart
+++ lib/models/review_model.dart
@@ -1,17 +1,15 @@
-import 'dart:html';
-
-class VeterinarianModel {
+class ReviewModel {
 
   int? id;
   String? review;
   int? veterinarian_id;
 
-  VeterinarianModel(this.id,
+  ReviewModel(this.id,
       this.review,
       this.veterinarian_id
       );
 
-  VeterinarianModel.fromJson(Map<String, dynamic> json){
+  ReviewModel.fromJson(Map<String, dynamic> json){
     id = json['id'];
     review = json['email'];
     veterinarian_id = json['veterinarian_id'];
